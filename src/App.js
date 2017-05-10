2c81f67b8a2aa59809e81620761cb6a09dc449d2
Bjarki Einarsson <bjarkieinars@gmail.com>
2017-05-10 22:29:51 +0000

    blehafooo

--- src/App.js
+++ src/App.js
@@ -5,19 +5,48 @@ import * as firebase from 'firebase';
 class App extends Component {
 
   constructor() {
-      super();
-      this.state = {
-        players: 'Loading',
-      };
+    super();
+    this.state = {
+      players: 'Benni',
+      inputValue: '',
+    };
   }
 
   componentDidMount() {
-    const rootRef = firebase.database().ref();
-    const playersRef = rootRef.child('players/player1/name');
-    playersRef.on('value', snap => {
-      this.setState({
-        players: snap.val()
-      });
+    var playersRef = firebase.database().ref("players/");
+
+    playersRef.on("child_added", function(data) {
+      var newPlayer = data.val();
+      console.log("name: " + newPlayer.name);
+      console.log("wins: " + newPlayer.wins);
+      console.log("losses: " + newPlayer.losses);
+    });
+
+    playersRef.on("child_changed", function(data) {
+      var player = data.val();
+      console.log("The updated player name is " + player.name);
+    });
+
+    // const playersRef = rootRef.child('players/player1/name');
+    // playersRef.on('value', snap => {
+    //   this.setState({
+    //     players: snap.val()
+    //   });
+    // });
+  }
+
+  setValue(field, e) {
+    // TODO
+  }
+
+  newPlayer(e) {
+    e.preventDefault();
+    var playersRef = firebase.database().ref("players/");
+
+    playersRef.push({
+      name: 'test',
+      wins: 0,
+      losses: 0
     });
   }
 
@@ -25,6 +54,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1>{this.state.players}</h1>
+        <form onSubmit={ (e) => this.newPlayer(e) }>
+          <input onChange={this.setValue.bind(this, 'inputValue')} placeholder='New Player Name' />
+          <button type="submit">Add</button>
+        </form>
       </div>
     );
   }
