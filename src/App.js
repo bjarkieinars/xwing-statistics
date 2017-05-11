de6565272463d9c0a3d0027da8277c434880751f
Bjarki Einarsson <bjarki.einarsson@landsbankinn.is>
2017-05-11 09:56:37 +0000

    create player ready

--- src/App.js
+++ src/App.js
@@ -2,29 +2,41 @@ import React, { Component } from 'react';
 import './App.css';
 import * as firebase from 'firebase';
 
+const winRatio = player => {
+  const games = player.wins + player.losses
+  return games > 0 ? +(Math.round((player.wins / games) + 'e+2') + 'e-2') : 0
+}
 class App extends Component {
 
   constructor() {
     super();
     this.state = {
-      players: 'Benni',
-      inputValue: '',
+      players: [],
     };
   }
 
   componentDidMount() {
     var playersRef = firebase.database().ref("players/");
 
-    playersRef.on("child_added", function(data) {
+    playersRef.on("child_added", (data) => {
       var newPlayer = data.val();
-      console.log("name: " + newPlayer.name);
-      console.log("wins: " + newPlayer.wins);
-      console.log("losses: " + newPlayer.losses);
+      this.setState(prev => {
+        prev.players.push(newPlayer)
+        prev.players.sort((a,b) => a.wins < b.wins)
+        return {players: prev.players}
+      })
     });
 
-    playersRef.on("child_changed", function(data) {
+    playersRef.on("child_changed", (data) => {
       var player = data.val();
-      console.log("The updated player name is " + player.name);
+      console.log(player);
+      this.setState(prev => {
+        const idx = prev.players.findIndex(p => p.name === player.name)
+        const newArray = [...prev.players.slice(0, idx), ...prev.players.slice(idx + 1, prev.players.length)]
+        newArray.push(player)
+        newArray.sort((a,b) => a.wins < b.wins)
+        return {players: newArray}
+      })
     });
 
     // const playersRef = rootRef.child('players/player1/name');
@@ -35,28 +47,46 @@ class App extends Component {
     // });
   }
 
-  setValue(field, e) {
-    // TODO
+  update() {
+
   }
 
   newPlayer(e) {
     e.preventDefault();
     var playersRef = firebase.database().ref("players/");
+    var newPlayerName = document.getElementById('newplayer').value;
 
     playersRef.push({
-      name: 'test',
+      name: newPlayerName,
       wins: 0,
-      losses: 0
+      losses: 0,
+      mov: 0
     });
+
+    document.getElementById('newplayer').value = '';
   }
 
   render() {
     return (
       <div className="App">
-        <h1>{this.state.players}</h1>
-        <form onSubmit={ (e) => this.newPlayer(e) }>
-          <input onChange={this.setValue.bind(this, 'inputValue')} placeholder='New Player Name' />
-          <button type="submit">Add</button>
+        {this.state.players.map(player =>
+          <div className='card' key={player.name}>
+            <h1 className='player-name'>{player.name}</h1>
+            <div className='stats-container'>
+              <div className='stats'>
+                <span>Games: {player.wins + player.losses}</span>
+                <span>Win rate: {winRatio(player)}</span>
+              </div>
+              <div className='stats'>
+                <span>Wins: {player.wins}</span>
+                <span>Losses: {player.losses}</span>
+              </div>
+            </div>
+          </div>
+        )}
+        <form className='add-player' onSubmit={ (e) => this.newPlayer(e) }>
+          <input className='input' id='newplayer' placeholder='New Player Name' />
+          <button className='button' type='submit'>Add</button>
         </form>
       </div>
     );
