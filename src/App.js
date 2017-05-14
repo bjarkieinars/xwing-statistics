715e57ae257cd120e82c6b18d1f7ca7daed5c147
Bjarki Einarsson <bjarkieinars@gmail.com>
2017-05-14 14:29:53 +0000

    first version of results

--- src/App.js
+++ src/App.js
@@ -22,33 +22,59 @@ class App extends Component {
       var newPlayer = data.val();
       this.setState(prev => {
         prev.players.push(newPlayer)
-        prev.players.sort((a,b) => a.wins < b.wins)
+        prev.players.sort((a,b) => a.wins > b.wins)
+        prev.players.reverse();
         return {players: prev.players}
       })
     });
 
     playersRef.on("child_changed", (data) => {
       var player = data.val();
-      console.log(player);
       this.setState(prev => {
         const idx = prev.players.findIndex(p => p.name === player.name)
         const newArray = [...prev.players.slice(0, idx), ...prev.players.slice(idx + 1, prev.players.length)]
         newArray.push(player)
-        newArray.sort((a,b) => a.wins < b.wins)
+        newArray.sort((a,b) => a.wins > b.wins)
+        newArray.reverse();
         return {players: newArray}
       })
     });
-
-    // const playersRef = rootRef.child('players/player1/name');
-    // playersRef.on('value', snap => {
-    //   this.setState({
-    //     players: snap.val()
-    //   });
-    // });
   }
 
-  update() {
+  newResult(e) {
+    e.preventDefault();
+    var resultsRef = firebase.database().ref("results/");
+    var playersRef = firebase.database().ref("players/");
+    var winnerName = document.getElementById('player1').value;
+    var looserName = document.getElementById('player2').value;
 
+    if (winnerName === looserName) {
+      alert('error 1 - same winner/looser');
+      return;
+    }
+
+    resultsRef.push({
+      winner: winnerName,
+      looser: looserName,
+      date: + new Date()
+    });
+
+    playersRef.once('value').then(function(snapshot) {
+      snapshot.forEach(function(childSnap) {
+        if (childSnap.child('name').val() === winnerName) {
+          var winnerRef = firebase.database().ref("players/" + childSnap.key);
+          winnerRef.update({
+            'wins': childSnap.child('wins').val() + 1
+          })
+        }
+        else if (childSnap.child('name').val() === looserName) {
+          var looserRef = firebase.database().ref("players/" + childSnap.key);
+          looserRef.update({
+            'losses': childSnap.child('losses').val() + 1
+          })
+        }
+      })
+    })
   }
 
   newPlayer(e) {
@@ -69,6 +95,28 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h2 className='header'>New Result</h2>
+        <form className='form' onSubmit={ (e) => this.newResult(e) }>
+          <div className='result'>
+            <div className='result-container'>
+              <label className='label'>Winner</label>
+              <select className='input' id='player1'>
+                {this.state.players.map(player =>
+                  <option key={player.name}>{player.name}</option>
+                )}
+              </select>
+            </div>
+            <div className='result-container'>
+              <label className='label'>Looser</label>
+              <select className='input' id='player2'>
+                {this.state.players.map(player =>
+                  <option key={player.name}>{player.name}</option>
+                )}
+              </select>
+            </div>
+          </div>
+          <button className='button' type='submit'>Add</button>
+        </form>
         {this.state.players.map(player =>
           <div className='card' key={player.name}>
             <h1 className='player-name'>{player.name}</h1>
@@ -84,10 +132,11 @@ class App extends Component {
             </div>
           </div>
         )}
-        <form className='add-player' onSubmit={ (e) => this.newPlayer(e) }>
+        <form className='form' onSubmit={ (e) => this.newPlayer(e) }>
           <input className='input' id='newplayer' placeholder='New Player Name' />
           <button className='button' type='submit'>Add</button>
         </form>
+
       </div>
     );
   }
